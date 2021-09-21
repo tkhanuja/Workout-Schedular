abeed202f0c3df0844e812bd3c1a48ff770af0b4
Tanvi Khanuja <tanvikhanuja21@gmail.com>
2021-09-21 15:59:17 -0700

    done

--- calander_control.py
+++ calander_control.py
@@ -1,26 +1,43 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome import options
+from selenium.webdriver.chrome.options import Options
 import time
 from passwords import email, password
- 
-options.Options.add_argument("--disable-web-security")
-options.Options.add_argument("--user-data-dir=true")
-options.Options.add_argument("--allow-running-insecure-content") 
 
-driver = webdriver.Chrome(executable_path="venv/bin/chromedriver")
 
+# o = Options() 
+# o.add_argument("--disable-web-security")
+# o.add_argument("--user-data-dir=true")
+# o.add_argument("--allow-running-insecure-content") 
+
+driver = webdriver.Chrome('venv/bin/chromedriver')
+
+
+
+driver.get('https://todoist.com/users/showlogin')
 
 driver.maximize_window()
-driver.get('https://calendar.google.com/calendar/u/0/r/month?tab=rc')
 
-# sign in
-# element = driver.find_element_by_id("yDmH0d")
-element = driver.find_element_by_xpath("//input[@id='identifierId']").send_keys(email)
-driver.find_element_by_id("identifierNext").click()
-   
+time.sleep(1)
+driver.find_element_by_xpath("//input[@id='email']").send_keys(email)
+
+time.sleep(1)
+driver.find_element_by_name('password').send_keys(password)
+
+time.sleep(1)
+driver.find_element_by_class_name('submit_btn.ist_button.ist_button_red.sel_login').click()
+
+time.sleep(1)
+driver.find_element_by_css_selector("button.plus_add_button").click()
+
+time.sleep(1)
+driver.find_element_by_css_selector('div.public-DraftStyleDefault-block.public-DraftStyleDefault-ltr').send_keys('Workout')
 
 
+time.sleep(1)
+driver.find_element_by_css_selector('textarea.task_editor__description_field.no-focus-marker').send_keys('Workout')
 
-     
+time.sleep(1)
+driver.find_element_by_css_selector('button.reactist_button.reactist_button--primary').click()
+
